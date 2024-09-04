7dbc0b8888defaaa5e42c2aa7db97be562dadff3
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-09-04 22:15:49 +0300

    files

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-// import FilesController from '../controllers/FilesController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -15,19 +15,19 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
 // upload files
-// router.post('/files', FilesController.postUpload);
-// router.get('/files/:id', FilesController.getShow);
-// router.get('/files', FilesController.getIndex);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 // publish and unpublish
-// router.put('/files/:id/publish', FilesController.putPublish);
-// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 // user
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 
 // file content
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
